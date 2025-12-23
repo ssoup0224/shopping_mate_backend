0ac94701e2d8005cc524e62241a4618bebf3fdc5
정성국 <ssoup0224@gmail.com>
2025-12-23 11:52:05 +0900

    init

---
+++ src/main/java/com/shopping_mate_backend/ShoppingMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopping_mate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopping_mate_backend/ShoppingMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping_mate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
